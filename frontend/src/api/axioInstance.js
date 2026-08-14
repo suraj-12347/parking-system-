15b030b806a14d56262a6c78969648af2bb6519a
Suraj Kushwah <6266988978sksingh@gmail.com>
2026-08-14 10:33:23 +0530

    setup

--- frontend/src/api/axioInstance.js
+++ frontend/src/api/axioInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+const axiosInstance = axios.create({ 
+  baseURL: "https://parking-system-2-kv3h.onrender.com/api",
 });
 
 // JWT automatically attach hoga
